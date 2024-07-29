b87f423d473304db0535e00752b119c6018afd7f
David H <davyd2h@gmail.com>
2024-07-29 16:06:40 -0500

    se crea interfaz bonita, y un poco de js para el botón de ingresar información

---
+++ public/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const textarea = document.querySelector('.input-observaciones');
+
+    textarea.addEventListener('input', function() {
+        this.style.height = 'auto'; // Reset the height
+        this.style.height = this.scrollHeight + 'px'; // Set the height based on the content
+    });
+});
