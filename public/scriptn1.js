4058192d68736246d17b1e6bb022d63a9afd2efd
David Hernandez <davyd2h@gmail.com>
2024-08-02 13:30:39 -0500

    en rama windo10 contador ok, recibe la información, almacenamiento probado, ok

--- public/scriptn1.js
+++ public/scriptn1.js
@@ -9,9 +9,9 @@ async function fetchTiempoTotalN1() {
         const cantidadIngresos = data.cantidadIngresos;
 
         document.getElementById('conteoDiario').textContent = 
-            `Tiempo Total: ${minutosGlobal} minutos y ${segundosGlobal} segundos, ` +
-            `AHT: ${minutosPromedio} minutos y ${segundosPromedio} segundos, ` +
-            `Cantidad: ${cantidadIngresos}`;
+            `AHT: ${minutosPromedio}.${segundosPromedio.toFixed()}, ` +
+            `Total: ${minutosGlobal}.${segundosGlobal.toFixed()}, ` +
+            `Interacciones: ${cantidadIngresos}`;
     } catch (error) {
         console.error('Error al obtener el tiempo total:', error);
         document.getElementById('conteoDiario').textContent = 'Error al cargar los datos';
